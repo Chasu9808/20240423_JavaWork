9925bf8163cb14f353b72cf1046e5cd236a3b7f3
Limchasu <mayblue20@naver.com>
2024-04-23 23:52:05 +0900

    202423 2교시 저장1 기초 예약어

---
+++ src/ex_240423_home/TestClass1.java
@@ -0,0 +1,14 @@
+package ex_240423_home;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		System.out.println("예를 들어 메인 시작입니다.");
+		for(int i = 0; i < 10; i++) {
+			System.out.println("I = :" + i);
+		}
+			System.out.println("for 문 종료");
+			System.out.println("main 종료");
+	}
+
+}
